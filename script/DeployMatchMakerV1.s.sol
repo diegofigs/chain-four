b947ff03552aad26aa9c3e0b07d8c8da7b2a60bc
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-06-26 21:48:29 -0400

    ci: set private key for test workflow

--- script/DeployMatchMakerV1.s.sol
+++ script/DeployMatchMakerV1.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {ConnectFourMatchMakerV1} from "src/ConnectFourMatchMakerV1.sol";
@@ -8,8 +8,10 @@ contract DeployMatchMakerV1 is Script {
     function run() external returns (ConnectFourMatchMakerV1) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        ConnectFourMatchMakerV1 mm = new ConnectFourMatchMakerV1();
+
+        ConnectFourMatchMakerV1 matchMaker = new ConnectFourMatchMakerV1();
+
         vm.stopBroadcast();
-        return mm;
+        return matchMaker;
     }
 }
